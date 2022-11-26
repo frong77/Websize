3fe3a138cc4f5e6ea64bf845911bc0f8b9fda366
chatmongcol <cdadwngs@gmail.com>
2022-11-26 12:38:18 +0700

    update

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>skill2565 day2</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include_once("includes/header.php");?>
+<?php include_once("includes/navbar.php");?>
+
+<div class="container my-3">
+    <div class="card shadow">
+
+        <div class="card-header">
+            <h1>Home page</h1>
+        </div>
+
+        <div class="card-body">
+            <h5 class="card-title">ระบบสั่งอาหารออนไลน์</h5>
+            <p class="card-text">  การเขียนโปรแกรมคอมพิวเตอร์เบื่องต้น</p>
+            <a class="btn btn-primary" href="#" >วท.ตราด</a>
+       </div>
+
+    </div>
+</div>
+
+<?php include_once("includes/footer.php");?>
